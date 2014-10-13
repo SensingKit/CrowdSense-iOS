26e1d64c5873c4d0b11df6c04ca3d5cf552900a2
Minos Katevas <minos.kat@gmail.com>
2014-10-13 17:18:49 +0100

    Add Round Button Implementation

--- CrowdSensing-iOS/CSViewController.m
+++ CrowdSensing-iOS/CSViewController.m
@@ -8,11 +8,24 @@
 
 #import "CSViewController.h"
 
+enum CSStartButtonMode : NSUInteger {
+    CSStartButtonStartMode,
+    CSStartButtonStopMode
+};
+
+enum CSPauseButtonMode : NSUInteger {
+    CSPauseButtonPauseMode,
+    CSPauseButtonResumeMode
+};
+
 @interface CSViewController ()
 
 @property (weak, nonatomic) IBOutlet CSRoundButton *startButton;
 @property (weak, nonatomic) IBOutlet CSRoundButton *pauseButton;
 @property (weak, nonatomic) IBOutlet CSRoundButton *syncButton;
+
+@property (nonatomic) enum CSStartButtonMode startButtonMode;
+@property (nonatomic) enum CSPauseButtonMode pauseButtonMode;
 
 @end
 
@@ -21,25 +34,99 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	
+    // Init Button Modes
+    self.startButtonMode = CSStartButtonStartMode;
+    self.pauseButtonMode = CSPauseButtonPauseMode;
 }
 
-- (void)didReceiveMemoryWarning
+- (void)setStartButtonMode:(enum CSStartButtonMode)startButtonMode
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    switch (startButtonMode) {
+        case CSStartButtonStartMode:
+            [self.startButton setTitle:@"Start" forState:UIControlStateNormal];
+            break;
+            
+        case CSStartButtonStopMode:
+            [self.startButton setTitle:@"Stop" forState:UIControlStateNormal];
+            break;
+            
+        default:
+            // Error
+            break;
+    }
+    
+    _startButtonMode = startButtonMode;
 }
 
 - (IBAction)startButtonAction:(CSRoundButton *)sender
 {
+    switch (self.startButtonMode) {
+        case CSStartButtonStartMode:
+            
+            NSLog(@"Start Action");
+            self.startButtonMode = CSStartButtonStopMode;
+            
+            break;
+            
+        case CSStartButtonStopMode:
+            
+            NSLog(@"Stop Action");
+            self.startButtonMode = CSStartButtonStartMode;
+            
+            break;
+            
+        default:
+            // Error
+            break;
+    }
+}
+
+- (void)setPauseButtonMode:(enum CSPauseButtonMode)pauseButtonMode
+{
+    switch (pauseButtonMode) {
+        case CSPauseButtonPauseMode:
+            [self.pauseButton setTitle:@"Pause" forState:UIControlStateNormal];
+            break;
+            
+        case CSPauseButtonResumeMode:
+            [self.pauseButton setTitle:@"Resume" forState:UIControlStateNormal];
+            break;
+            
+        default:
+            // Error
+            break;
+    }
+    
+    _pauseButtonMode = pauseButtonMode;
 }
 
 - (IBAction)pauseButtonAction:(CSRoundButton *)sender
 {
+    switch (self.pauseButtonMode) {
+        case CSPauseButtonPauseMode:
+            
+            NSLog(@"Pause Action");
+            self.pauseButtonMode = CSPauseButtonResumeMode;
+            
+            break;
+            
+        case CSPauseButtonResumeMode:
+            
+            NSLog(@"Resume Action");
+            self.pauseButtonMode = CSPauseButtonPauseMode;
+            
+            break;
+            
+        default:
+            // Error
+            break;
+    }
 }
 
 - (IBAction)syncButtonAction:(CSRoundButton *)sender
 {
+    NSLog(@"Sync Action");
 }
 
 @end
