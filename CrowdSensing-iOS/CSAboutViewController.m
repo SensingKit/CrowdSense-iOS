3e8ed5f582a3844833aac8aa275188831562f50d
Minos Katevas <minos.kat@gmail.com>
2015-08-28 21:02:35 +0100

    Fix issues with About screen; Update text in About screen.

--- CrowdSensing-iOS/CSAboutViewController.m
+++ CrowdSensing-iOS/CSAboutViewController.m
@@ -10,29 +10,17 @@
 
 @interface CSAboutViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextView *textView;
+
 @end
 
 @implementation CSAboutViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    [self.textView setContentOffset:CGPointZero animated:NO];
 }
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 - (IBAction)DoneAction:(id)sender
 {
