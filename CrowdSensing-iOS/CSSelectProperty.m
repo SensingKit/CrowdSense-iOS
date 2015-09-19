d0fa0021f78c1ecf2bf2627e5af75fc2b9039450
Minos Katevas <minos.kat@gmail.com>
2015-09-19 21:02:09 +0100

    Add checkmark on selected properties

--- CrowdSensing-iOS/CSSelectProperty.m
+++ CrowdSensing-iOS/CSSelectProperty.m
@@ -14,12 +14,6 @@
 
 @implementation CSSelectProperty
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    
-    //self.selectedValue
-}
-
 
 #pragma mark - Table view data source
 
@@ -39,16 +33,38 @@
     
     cell.textLabel.text = self.elements[indexPath.row];
     
+    if (self.selectedIndex == indexPath.row)
+    {
+        cell.accessoryType = UITableViewCellAccessoryCheckmark;
+    }
+    
     return cell;
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
+    [self deselectRowWithIndex:self.selectedIndex];
+    [self selectRowWithIndex:indexPath.row];
+    
     self.selectedIndex = indexPath.row;
     
     [self.delegate selectPropertyWithIdentifier:self.identifier withIndex:indexPath.row withValue:self.elements[indexPath.row]];
     
     [self.navigationController popViewControllerAnimated:YES];
+}
+
+- (void)selectRowWithIndex:(NSUInteger)index
+{
+    NSIndexPath *indexPath = [NSIndexPath indexPathForRow:index inSection:0];
+    UITableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
+    cell.accessoryType = UITableViewCellAccessoryCheckmark;
+}
+
+- (void)deselectRowWithIndex:(NSUInteger)index
+{
+    NSIndexPath *indexPath = [NSIndexPath indexPathForRow:index inSection:0];
+    UITableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
+    cell.accessoryType = UITableViewCellAccessoryNone;
 }
 
 @end
