977b2cc37a07604ecc5321d3f1c67d26ec15a6f2
Minos Katevas <minos.kat@gmail.com>
2017-03-11 18:36:55 +0000

    Add support for data upload

--- CrowdSensing-iOS/CSSubmitDataViewController.m
+++ CrowdSensing-iOS/CSSubmitDataViewController.m
@@ -7,12 +7,14 @@
 //
 
 #import "CSSubmitDataViewController.h"
+@import AFNetworking;
 
 @interface CSSubmitDataViewController ()
 
 @property (weak, nonatomic) IBOutlet UIProgressView *dataProgressView;
 @property (weak, nonatomic) IBOutlet UILabel *dataProgressLabel;
 @property (weak, nonatomic) IBOutlet UIButton *finishButton;
+@property (weak, nonatomic) IBOutlet UITextView *statusTextView;
 
 @end
 
@@ -21,6 +23,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    [self uploadData];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -42,6 +46,59 @@
 {
     // Should be disabled initially
     [self dismissViewControllerAnimated:YES completion:NULL];
+}
+
+- (void)uploadData
+{
+    NSMutableURLRequest *request = [[AFHTTPRequestSerializer serializer] multipartFormRequestWithMethod:@"POST" URLString:@"https://sensingkit.herokuapp.com" parameters:nil constructingBodyWithBlock:^(id<AFMultipartFormData> formData) {
+        
+        // Set password
+        [formData appendPartWithFormData:[@"super" dataUsingEncoding:NSUTF8StringEncoding] name:@"password"];
+        
+        // Set data
+        //NSURL *dataUrl = [NSURL URLWithString:@"..."];
+        //[formData appendPartWithFileURL:dataUrl name:@"uploadedFile" fileName:@"filename.zip" mimeType:@"application/zip" error:nil];
+        
+        // Tmp data for testing reasons
+        NSData *data = [NSData dataWithContentsOfURL:[NSURL URLWithString:@"https://minoskt.github.io/media/projects/robothespian_thumb.jpg"]];
+        [formData appendPartWithFileData:data name:@"uploadedFile" fileName:@"filename.jpg" mimeType:@"image/jpeg"];
+        
+    } error:nil];
+    
+    AFURLSessionManager *manager = [[AFURLSessionManager alloc] initWithSessionConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]];
+    manager.responseSerializer = [AFHTTPResponseSerializer serializer];
+    
+    NSURLSessionUploadTask *uploadTask;
+    uploadTask = [manager
+                  uploadTaskWithStreamedRequest:request
+                  progress:^(NSProgress * _Nonnull uploadProgress) {
+                      
+                      // Dispatch to the main queue for UI updates
+                      dispatch_async(dispatch_get_main_queue(), ^{
+                          
+                          // Update the progress view
+                          [self.dataProgressView setProgress:uploadProgress.fractionCompleted];
+                          
+                          // Update label
+                          NSUInteger percent = 100 * uploadProgress.fractionCompleted;
+                          self.dataProgressLabel.text = [NSString stringWithFormat:@"%lu%% completed", (unsigned long)percent];
+                      });
+                  }
+                  completionHandler:^(NSURLResponse * _Nonnull response, id  _Nullable responseObject, NSError * _Nullable error) {
+                      
+                      if (error)
+                      {
+                          NSLog(@"Error: %@", error);
+                      }
+                      else
+                      {
+                          // Enable finish button and show status view
+                          self.statusTextView.hidden = NO;
+                          self.finishButton.enabled = YES;
+                      }
+                  }];
+    
+    [uploadTask resume];
 }
 
 @end
